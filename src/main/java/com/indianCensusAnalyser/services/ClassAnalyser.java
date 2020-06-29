02d23ca0428899477d324f32685f6a14f3cee613
Sudhanshu Ghinmine <sghinmine54@gmail.com>
2020-06-30 05:11:42 +0530

    REFACTORED To eleminate unused code

--- src/main/java/com/indianCensusAnalyser/services/ClassAnalyser.java
+++ src/main/java/com/indianCensusAnalyser/services/ClassAnalyser.java
@@ -44,38 +44,4 @@ public class ClassAnalyser {
         }
         return namOfEateries;
     }
-//    public int loadIndiaCensusData(String csvFilePath) throws CensusAnalyserException {
-//        int namOfEateries = 0;
-//        try {
-//            Reader reader = null;
-//            Iterator<CSVStateCensus> censusCSVIterator = null;
-//            try {
-//                reader = Files.newBufferedReader(Paths.get(csvFilePath));
-//                CsvToBeanBuilder<CSVStateCensus> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
-//                csvToBeanBuilder.withType(CSVStateCensus.class);
-//                csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
-//                CsvToBean<CSVStateCensus> csvToBean = csvToBeanBuilder.build();
-//                censusCSVIterator = csvToBean.iterator();
-//            } catch (NoSuchFileException e) {
-//                throw new CensusAnalyserException(e.getMessage(),
-//                        CensusAnalyserException.ExceptionType.FILE_TYPE_INCORRECT);
-//            } catch (RuntimeException e) {
-//                throw new CensusAnalyserException(e.getMessage(),
-//                        CensusAnalyserException.ExceptionType.INCORRECT_FILE);
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//            namOfEateries = 0;
-//            while (censusCSVIterator.hasNext()) {
-//                namOfEateries++;
-//                CSVStateCensus censusData = censusCSVIterator.next();
-//            }
-//        } catch (CensusAnalyserException e) {
-//            throw e;
-//        }catch (RuntimeException e) {
-//            throw new CensusAnalyserException(e.getMessage(),
-//                    CensusAnalyserException.ExceptionType.DELIMITER_INCORRECT);
-//        }
-//        return namOfEateries;
-//    }
 }
