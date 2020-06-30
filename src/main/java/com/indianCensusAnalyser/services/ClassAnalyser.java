c53ded55c384eb9ef69298bf99bf50d48f2597b9
Sudhanshu Ghinmine <sghinmine54@gmail.com>
2020-07-01 02:48:59 +0530

    REFACTORED to handle single responsebility principle by segrigating csvFileIterator function in separate class

--- src/main/java/com/indianCensusAnalyser/services/ClassAnalyser.java
+++ src/main/java/com/indianCensusAnalyser/services/ClassAnalyser.java
@@ -1,7 +1,6 @@
 package com.indianCensusAnalyser.services;
 
 import com.indianCensusAnalyser.exceptions.CSVAnalyserException;
-
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
@@ -12,8 +11,6 @@ import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
 import java.util.Iterator;
 
-import java.util.stream.StreamSupport;
-
 public class ClassAnalyser {
     public <E> int loadIndianStateData(String csvFilePath, Class<E> csvClass) throws CSVAnalyserException {
         int numOfEntries = 0;
@@ -21,7 +18,7 @@ public class ClassAnalyser {
             Iterator<E> csvIterator = null;
             try {
                 Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
-                csvIterator = this.csvFileIterator(reader, csvClass);
+                csvIterator = new OpenCSVBuilder().csvFileIterator(reader, csvClass);
             } catch (NoSuchFileException e) {
                 throw new CSVAnalyserException
                         ("No such file exists",
@@ -29,16 +26,14 @@ public class ClassAnalyser {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
             numOfEntries = this.getCount(csvIterator);
-
         } catch (CSVAnalyserException e) {
             throw e;
         }
         return numOfEntries;
     }
 
-    private int getCount (Iterator csvIterator) throws CSVAnalyserException {
+    private <E> int getCount (Iterator<E> csvIterator) throws CSVAnalyserException {
         int count = 0;
         try {
             while (csvIterator.hasNext()) {
@@ -50,20 +45,5 @@ public class ClassAnalyser {
                     ("File contains wrong delimiter", CSVAnalyserException.ExceptionType.DELIMITER_INCORRECT);
         }
         return count;
-    }
-
-    private <E> Iterator<E> csvFileIterator(Reader reader, Class<E> csvClass) throws CSVAnalyserException {
-        try {
-            CsvToBeanBuilder<E> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
-            csvToBeanBuilder.withType(csvClass);
-            csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
-            CsvToBean<E> csvToBean = csvToBeanBuilder.build();
-            Iterator<E> csvIterator = csvToBean.iterator();
-            return csvIterator;
-        } catch (RuntimeException e) {
-            throw new CSVAnalyserException
-                    ("File type is incorrect or incorrect header",
-                            CSVAnalyserException.ExceptionType.INCORRECT_FILE_OR_HEADER);
-        }
     }
 }
---
+++ src/main/java/com/indianCensusAnalyser/services/OpenCSVBuilder.java
@@ -0,0 +1,25 @@
+package com.indianCensusAnalyser.services;
+
+import com.indianCensusAnalyser.exceptions.CSVAnalyserException;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.Reader;
+import java.util.Iterator;
+
+public class OpenCSVBuilder {
+    public <E> Iterator<E> csvFileIterator(Reader reader, Class<E> csvClass) throws CSVAnalyserException {
+        try {
+            CsvToBeanBuilder<E> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+            csvToBeanBuilder.withType(csvClass);
+            csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+            CsvToBean<E> csvToBean = csvToBeanBuilder.build();
+            Iterator<E> csvIterator = csvToBean.iterator();
+            return csvIterator;
+        } catch (RuntimeException e) {
+            throw new CSVAnalyserException
+                    ("File type is incorrect or incorrect header",
+                            CSVAnalyserException.ExceptionType.INCORRECT_FILE_OR_HEADER);
+        }
+    }
+}
